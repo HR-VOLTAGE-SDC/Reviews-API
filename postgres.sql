249d85681d50c5f1ebfb92049348b5a4df57f852
Eunice Kim <euniceyunjkim@gmail.com>
2022-04-20 14:07:59 -0700

    Changes to postgres tables.

--- postgres.sql
+++ postgres.sql
@@ -3,41 +3,42 @@ DROP DATABASE IF EXISTS reviews_API;
 CREATE DATABASE reviews_API;
 
 DROP TABLE IF EXISTS reviews;
-DROP TABLE IF EXISTS photos;
-DROP TABLE IF EXISTS characteristics;
-DROP TABLE IF EXISTS review_characteristics;
 
 CREATE TABLE reviews (
-id BIGSERIAL NOT NULL UNIQUE AUTO_INCREMENT,
-product_id INT,
-rating SMALLINT CONSTRAINT check_rating CHECK (rating > 0 AND rating < 6),
-summary VARCHAR(60),
-recommend BOOLEAN,
-response ,
-body VARCHAR(1000) CONSTRAINT check_min CHECK (char_length(body) >= 50),
-review_date TIMESTAMP,
-reviewer_name VARCHAR(60),
-reviewer_email VARCHAR(60),
-helpfulness BOOLEAN,
-reported BOOLEAN,
-//ask Eric about whether photos is necessary //here or if I can combine both
-photos VARCHAR ARRAY[5],
+id SERIAL PRIMARY KEY UNIQUE,
+product_id INT NOT NULL,
+rating SMALLINT CONSTRAINT check_rating CHECK (rating > 0 AND rating < 6) NOT NULL,
+summary VARCHAR(60) NOT NULL,
+recommend BOOLEAN NOT NULL,
+response VARCHAR(1000) CONSTRAINT check_res CHECK(char_length(response) >= 50) NOT NULL,
+body VARCHAR(1000) CONSTRAINT check_min CHECK (char_length(body) >= 50) NOT NULL,
+review_date TIMESTAMP NOT NULL,
+reviewer_name VARCHAR(255) NOT NULL,
+reviewer_email VARCHAR(255) NOT NULL,
+helpfulness INT NOT NULL,
+reported BOOLEAN NOT NULL
 );
+
+DROP TABLE IF EXISTS photos;
 
 CREATE TABLE photos (
-id BIGSERIAL NOT NULL UNIQUE AUTO_INCREMENT,
+id SERIAL PRIMARY KEY UNIQUE,
 review_id INT NOT NULL,
-photo_url VARCHAR,
+url VARCHAR(255) NOT NULL
 );
+
+DROP TABLE IF EXISTS characteristics;
 
 CREATE TABLE characteristics (
-id BIGSERIAL NOT NULL UNIQUE AUTO_INCREMENT,
+id SERIAL PRIMARY KEY NOT NULL UNIQUE,
 product_id INT NOT NULL,
-characteristics_name VARCHAR(50),
+characteristics_name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE review_characteristics (
-id BIGSERIAL NOT NULL UNIQUE AUTO_INCREMENT,
+DROP TABLE IF EXISTS characteristics_reviews;
+
+CREATE TABLE characteristics_reviewss (
+id SERIAL PRIMARY KEY NOT NULL UNIQUE,
 review_id INT NOT NULL,
 characteristic_id INT NOT NULL,
 characteristic_rating SMALLINT CONSTRAINT check_char_rating CHECK (characteristic_rating > 0 AND characteristic_rating < 6),
@@ -45,6 +46,6 @@ characteristic_rating SMALLINT CONSTRAINT check_char_rating CHECK (characteristi
 
 ALTER TABLE photos ADD FOREIGN KEY (review_id) REFERENCES reviews(id);
 
-ALTER TABLE review_characteristics ADD FOREIGN KEY (review_id) REFERENCES reviews(id);
+ALTER TABLE characteristics_reviews ADD FOREIGN KEY (review_id) REFERENCES reviews(id);
 
-ALTER TABLE review_characteristics ADD FOREIGN KEY (characteristics_id) REFERENCES characteristics(id);
+ALTER TABLE characteristics_reviews ADD FOREIGN KEY (characteristics_id) REFERENCES characteristics(id);
